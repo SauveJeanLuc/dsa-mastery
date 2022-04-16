75e2a27d05a762150174ee75de5cd6ce0c175dee
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-16 23:54:02 +0200

    factorial function impl

--- c++/recursion.cpp
+++ c++/recursion.cpp
@@ -8,4 +8,16 @@ int main() {
 
     cout << "Enter a non-negative number: "
     cin >> n;
+
+    result = factorial(n);
+    cout << "Factorial of " << n << " = " << result;
+    return 0;
+}
+
+int factorial(int n){
+    if (n > 1) {
+        return n * factorial(n - 1);
+    } else {
+        return 1;
+    }
 }

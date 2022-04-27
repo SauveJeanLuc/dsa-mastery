0cc1051d9aa89635263d48ef356b61e0fcd419d4
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-27 09:32:20 +0200

    defining C++ Fundamental DataTypes

--- c++/programiz/datatypes.cpp
+++ c++/programiz/datatypes.cpp
@@ -1,3 +1,30 @@
+/*
+    Fundamental Data Types
+
+    | Data Type  |     Meaning   |  Size in Bytes |
+
+    |  short int | Short Integer |     2 bytes    |
+
+    |    int     |    Integer    |     4 bytes    |
+
+    |   float    | Floating-Point|     4 bytes    |
+
+    |   double   | Double F-Point|     8 bytes    |
+
+    |    char    |    Character  |     1 byte     |
+
+    |  wchar_t   | Wide Character|     2 bytes    |
+
+    |    bool    |    Boolean    |     1 byte     |
+
+    |    void    |     Empty     |     0 byte     |
+
+
+    Remember: Bytes = 8 Bits.
+    A bit can only hold 0 or 1.
+
+*/
+
 #include<iostream>
 
 int main(){

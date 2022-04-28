8097393b34d49138f52cbcc0081ecc5c9cc08e92
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-28 10:37:05 +0200

    improved string output

--- c++/src/tutorial/BasicIO/stringOutput.cpp
+++ c++/src/tutorial/BasicIO/stringOutput.cpp
@@ -2,12 +2,13 @@
 using namespace std; // Namespace containing std
 
 int main() {
-    /*
-    *
-    *
-    *
-    */
-    cout << " Printing String in double quotes";
+    int num1 = 70;
+    double num2 = 256.783;
+    char character = 'A';
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << "character: " << character << endl;
 
     return 0; // Exit Status 
 }

989146ffb03d05cc49302cb8cbbc3f01f6801248
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-28 13:44:40 +0200

    implicit type conversion

---
+++ c++/src/main/TypeConversion/implicit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int intValue = 10;
+
+    double doubleValue;
+
+    doubleValue = intValue;
+
+    cout << "Integer value: " << intValue << endl;
+    cout << "Double value " << doubleValue << endl;
+}

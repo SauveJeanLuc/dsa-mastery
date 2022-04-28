4bd32195b3b6bc4ca4945ae50f615617366e55e1
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-28 10:25:43 +0200

    move files to appropriate directory

---
+++ c++/src/tutorial/BasicIO/stringOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // Header File
+using namespace std; // Namespace containing std
+
+int main() {
+    /*
+    *
+    *
+    *
+    */
+    cout << " Printing String in double quotes";
+
+    return 0; // Exit Status 
+}

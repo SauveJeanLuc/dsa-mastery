67e099cc1d39521cad89a27779a32c67eb0bffb4
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-29 09:33:45 +0200

    depicting arithmetic operators

---
+++ c++/src/main/Operators/arithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long double a, b;
+
+    a = 3.1415926535897932384626433832795028L;
+    b = 2.5;
+
+    cout << "Sum: " << (a + b) << endl;
+
+    cout << "Difference: " << (a - b) << endl;
+
+    cout << "Product: " << (a*b) << endl;
+
+    cout << "Division: " << (a/b) << endl;
+
+    cout << "Modulo: " << (a%b) << endl;
+}

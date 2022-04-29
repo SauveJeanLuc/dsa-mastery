e81dcfd8da5cc33c0466fb1ee485b30715d4a951
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-04-29 09:27:02 +0200

    the 6 types of operators in C++

---
+++ c++/src/main/Operators/theory.cpp
@@ -0,0 +1,11 @@
+/*
+    Types of Operators in C++
+
+    1. Arithmetic Operators
+    2. Assignment Operators
+    3. Relational Operators
+    4. Logical Operators
+    5. Bitwise Operators
+    6. Other Operators
+
+*/

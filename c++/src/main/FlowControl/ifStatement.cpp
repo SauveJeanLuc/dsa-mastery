e20c0ad4031df90f7a2eb449eb2888477255a98b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-02 21:59:35 +0200

    If Statement

---
+++ c++/src/main/FlowControl/ifStatement.cpp
@@ -0,0 +1,21 @@
+// Program to print positive number entered by the user
+// If the user enters a negative number, it is skipped
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int positiveNumber;
+
+    cout << "Please Enter any Number: " ;
+    cin >> positiveNumber;
+
+    if(positiveNumber > 0){
+        cout << "Congratulations you have entered a positive number. ";
+    }
+
+    // Ommitting Bracelets in the program
+
+    if ()
+
+}

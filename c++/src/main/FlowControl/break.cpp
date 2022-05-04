275c5c31a8fe884b6b0b798fc74e146d024a9d8b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-04 15:24:43 +0200

    break

---
+++ c++/src/main/FlowControl/break.cpp
@@ -0,0 +1,27 @@
+// program to find the sum of positive numbers
+// if the user enters a negative numbers, break ends the loop
+// the negative number entered is not added to sum
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+    int result = 0;
+
+    while(true){
+        cout << "Please enter any number: ";
+        cin >> number;
+
+        if(number < 0){
+            break;
+        }
+
+        result + = number;
+
+    }
+
+    cout << "Sum: " << result << endl;
+
+}

1b4810463a399669d44fff5202888ccb4889a43a
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-05 11:04:29 +0200

    refined calc function

--- c++/src/main/Functions/calcFunction.cpp
+++ c++/src/main/Functions/calcFunction.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 
-double calc(int firstNumber , int secondNumber, char &operator){
-    switch (operator){
+double calculator(int firstNumber , int secondNumber, char* signOperator);
+
+
+int main() {
+
+    int firstNumber = 45;
+    int secondNumber = 100;
+    char add = '+';
+    char sub = '-';
+    char mult = '*';
+    char divid = '/';
+
+    cout << "When You Add: " << calculator(firstNumber, secondNumber, &add) << endl;
+    cout << "When You Subtract: " << calculator(firstNumber, secondNumber, &sub) << endl;
+    cout << "When You Multiply: " << calculator(firstNumber, secondNumber, &mult) << endl;
+    cout << "When You Divide: " << calculator(firstNumber, secondNumber, &divid) << endl;
+    
+}
+
+double calculator(int firstNumber , int secondNumber, char* signOperator){
+    switch (*signOperator){
         case '-':
             return firstNumber - secondNumber;
         case '+':
@@ -12,19 +31,9 @@ double calc(int firstNumber , int secondNumber, char &operator){
             return firstNumber * secondNumber;
         case '/':
             return firstNumber / secondNumber;
+        default:
+            return 0.0;
     }
-}
 
-
-int main() {
-
-    int firstNumber = 45;
-    int secondNumber = 100;
-
-    cout << "When You Add: " << calc(firstNumber, secondNumber, '+') << endl;
-    cout << "When You Subtract: " << calc(firstNumber, secondNumber, '-') << endl;
-    cout << "When You Multiply: " << calc(firstNumber, secondNumber, '*') << endl;
-    cout << "When You Divide: " << calc(firstNumber, secondNumber, '/') << endl;
-
-
+    return 0.0;
 }

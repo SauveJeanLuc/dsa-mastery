e4198635419cae3ab40728a65b4097b768a507a5
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-09 07:35:58 +0200

    multi-dimensional array declaration

---
+++ c++/src/main/Array/multidimensional.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Declaration: 
+
+int main () {
+    
+    // Two-Dimensional Array:
+
+    int twoDimensional[2][3] = { {0, 1, 2}, {1, 1, 2} };
+
+    // Three Dimensional Array:
+
+    int threeDimensional[2][3][4] = {
+        { { 3, 4, 2, 3 } , { 0, -3, 9, 11 }, { 23, 12, 23 ,2 } },
+        { { 13, 4, 56, 3 } , { 5, 9, 3, 5 }, {5, 1, 4, 9} }
+    };
+
+
+}

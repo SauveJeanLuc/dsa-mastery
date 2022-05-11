cfb8f28a61cd94604591352482f65c1c5ad24c77
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-11 07:53:18 +0200

    more class manipulations

--- c++/src/main/ClassesAndObjects/class.cpp
+++ c++/src/main/ClassesAndObjects/class.cpp
@@ -8,7 +8,7 @@ enum fileType {
 class FileOperator {
     private:
         fileType type;
-        string size;
+        float size;
     public: 
         string content;
 
@@ -27,9 +27,40 @@ class FileOperator {
         }
 
         //Parameterized Constructor
-        
-        FileOperator(fileType type, string size){
-            this.type = type;
-            this.size = size;
+
+        FileOperator(fileType type, float size){
+            this->type = type;
+            this->size = size;
         }
+
+        // Copy Constructor
+
+        FileOperator(FileOperator &obj){
+            content = obj.content;
+        }
+
+
+        float getFileSize(){
+            return size;
+        }
+
+};
+
+int main(){
+
+    FileOperator studentsList(XLSX, 340);
+    studentsList.content = "Me, You , She";
+
+    FileOperator teachersList(DOCX, 940);
+
+    //Using Copy constructor
+    //TODO: Inspect why all elements are being copied
+    
+    teachersList = studentsList;
+    
+
+    cout << "Student List File Size: " << studentsList.getFileSize() << endl;
+    cout << "Teachers List File Size: " << teachersList.getFileSize() << endl;
+    cout << "Teachers List File Content: " << teachersList.content << endl;
+
 }

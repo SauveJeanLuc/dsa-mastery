3138314292f43338095bd8cebb5e64a3dd4883a9
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-18 16:17:35 +0200

    accessmodifiers

---
+++ c++/src/main/Inheritance/accessmodifiers.cpp
@@ -0,0 +1,15 @@
+/*
+
+    Specifiers | Within Same Class |   In Derived Class |   Outside the Class
+
+    Private         Yes                     No                  No
+
+    Protected       Yes                     Yes                 No
+
+    Public          Yes                     Yes                 Yes
+
+
+
+
+
+*/

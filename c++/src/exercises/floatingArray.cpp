e2d94c0465d0ac749e2e949878e52e221d52adc0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-24 17:54:54 +0200

    arrays v1

---
+++ c++/src/exercises/floatingArray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float fiveHardCoded[] = {6.5, 7.983, 98.3, 343.34, 341.00};
+
+    cout << "Secong Element: " << fiveHardCoded[1] << endl;
+    
+}

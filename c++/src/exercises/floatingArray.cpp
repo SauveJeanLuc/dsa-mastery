168be6789a0ac6042ceb955d602e3027f65c4c88
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-05-24 18:07:56 +0200

    refine array v1

--- c++/src/exercises/floatingArray.cpp
+++ c++/src/exercises/floatingArray.cpp
@@ -5,6 +5,6 @@ int main() {
 
     float fiveHardCoded[] = {6.5, 7.983, 98.3, 343.34, 341.00};
 
-    cout << "Secong Element: " << fiveHardCoded[1] << endl;
-    
+    cout << "Second Element: " << fiveHardCoded[1] << endl;
+
 }
